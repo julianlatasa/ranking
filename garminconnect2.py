7bd519fb8c616595ba6c16c844d17b55cb741441
Julian Latasa <julianlatasa@gmail.com>
2022-02-09 01:10:43 -0300

    fixed connections

--- garminconnect2.py
+++ garminconnect2.py
@@ -8,97 +8,11 @@ from typing import Any, Dict
 
 import cloudscraper
 
+from garminconnect import ApiClient, GarminConnectAuthenticationError
+
 logger = logging.getLogger(__name__)
 
-
-class ApiClient:
-    """Class for a single API endpoint."""
-
-    default_headers = {
-        # 'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2'
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"
-
-    }
-
-    def __init__(self, session, baseurl, headers=None, aditional_headers=None):
-        """Return a new Client instance."""
-        self.session = session
-        self.baseurl = baseurl
-
-        if headers:
-            self.headers = headers
-        else:
-            self.headers = self.default_headers.copy()
-
-        if aditional_headers:
-            self.headers.update(aditional_headers)
-
-    def url(self, addurl=None):
-        """Return the url for the API endpoint."""
-
-        path = f"https://{self.baseurl}"
-        if addurl is not None:
-            path += f"/{addurl}"
-
-        return path
-
-    def get(self, addurl, aditional_headers=None, params=None):
-        """Make an API call using the GET method."""
-        total_headers = self.headers.copy()
-        if aditional_headers:
-            total_headers.update(aditional_headers)
-        url = self.url(addurl)
-
-        logger.debug("URL: %s", url)
-        logger.debug("Headers: %s", total_headers)
-
-        try:
-            response = self.session.get(url, headers=total_headers, params=params)
-            response.raise_for_status()
-            # logger.debug("Response: %s", response.content)
-            return response
-        except Exception as err:
-            logger.debug("Response in exception: %s", response.content)
-            if response.status_code == 429:
-                raise GarminConnectTooManyRequestsError("Too many requests") from err
-            if response.status_code == 401:
-                raise GarminConnectAuthenticationError("Authentication error") from err
-            if response.status_code == 403:
-                raise GarminConnectConnectionError(f"Forbidden url: {url}") from err
-
-            raise GarminConnectConnectionError(err) from err
-
-    def post(self, addurl, aditional_headers, params, data):
-        """Make an API call using the POST method."""
-        total_headers = self.headers.copy()
-        if aditional_headers:
-            total_headers.update(aditional_headers)
-        url = self.url(addurl)
-
-        logger.debug("URL: %s", url)
-        logger.debug("Headers: %s", total_headers)
-        logger.debug("Data: %s", data)
-
-        try:
-            response = self.session.post(
-                url, headers=total_headers, params=params, data=data
-            )
-            response.raise_for_status()
-            # logger.debug("Response: %s", response.content)
-            return response
-        except Exception as err:
-            logger.debug("Response in exception: %s", response.content)
-            if response.status_code == 429:
-                raise GarminConnectTooManyRequestsError("Too many requests") from err
-            if response.status_code == 401:
-                raise GarminConnectAuthenticationError("Authentication error") from err
-            if response.status_code == 403:
-                raise GarminConnectConnectionError(f"Forbidden url: {url}") from err
-
-            raise GarminConnectConnectionError(err) from err
-
-
-class Garmin:
+class Garmin2:
     """Class for fetching data from Garmin Connect."""
 
     def __init__(self, email, password, is_cn=False):
@@ -571,11 +485,11 @@ class Garmin:
         """
         activity_id = str(activity_id)
         urls = {
-            Garmin.ActivityDownloadFormat.ORIGINAL: f"{self.garmin_connect_fit_download}/{activity_id}",
-            Garmin.ActivityDownloadFormat.TCX: f"{self.garmin_connect_tcx_download}/{activity_id}",
-            Garmin.ActivityDownloadFormat.GPX: f"{self.garmin_connect_gpx_download}/{activity_id}",
-            Garmin.ActivityDownloadFormat.KML: f"{self.garmin_connect_kml_download}/{activity_id}",
-            Garmin.ActivityDownloadFormat.CSV: f"{self.garmin_connect_csv_download}/{activity_id}",
+            Garmin2.ActivityDownloadFormat.ORIGINAL: f"{self.garmin_connect_fit_download}/{activity_id}",
+            Garmin2.ActivityDownloadFormat.TCX: f"{self.garmin_connect_tcx_download}/{activity_id}",
+            Garmin2.ActivityDownloadFormat.GPX: f"{self.garmin_connect_gpx_download}/{activity_id}",
+            Garmin2.ActivityDownloadFormat.KML: f"{self.garmin_connect_kml_download}/{activity_id}",
+            Garmin2.ActivityDownloadFormat.CSV: f"{self.garmin_connect_csv_download}/{activity_id}",
         }
         if dl_fmt not in urls:
             raise ValueError(f"Unexpected value {dl_fmt} for dl_fmt")
@@ -663,13 +577,3 @@ class Garmin:
         self.modern_rest_client.get(self.garmin_connect_logout)
 
 
-class GarminConnectConnectionError(Exception):
-    """Raised when communication ended in error."""
-
-
-class GarminConnectTooManyRequestsError(Exception):
-    """Raised when rate limit is exceeded."""
-
-
-class GarminConnectAuthenticationError(Exception):
-    """Raised when authentication is failed."""
