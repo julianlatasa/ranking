f08df09a603325183c698ec0f9aa7562ed011ae6
Julian Latasa <julianlatasa@gmail.com>
2022-02-10 12:51:31 -0300

    Version con todo incluido stream

--- app.py
+++ app.py
@@ -17,10 +17,23 @@ from singletonGarmin import SingletonGarmin
 
 from flask import Flask, request, render_template, jsonify, session
 
+from time import sleep
+from math import sqrt
+
 app = Flask(__name__, template_folder='./')
 apiGarmin = SingletonGarmin()
 app.secret_key = 'GarminConnect'
 cache = {}
+
+
+@app.route("/stream")
+def stream():
+    def generate():
+        for i in range(500):
+            yield "{}\n".format(sqrt(i))
+            sleep(1)
+
+    return app.response_class(generate(), mimetype="text/plain")
 
 @app.route('/')
 def hello_world():
@@ -332,6 +345,141 @@ def query():
     return result
 
 
+@app.route('/ranking', methods=['POST'])
+def ranking():
+    if request.method == 'POST':
+        params = request.get_json()
+        usuario, password = params['usuario'].strip(), params['password'].strip()
+        fecha = params.get('fecha', "").strip()
+    def generate():
+        yield "Iniciando consulta\n" 
+            
+        if ((not usuario) or (not password)):
+            return "No se ingreso un usuario o una clave",403 
+        
+        if (not fecha):
+            return "No se ingreso una fecha",403
+
+        try:
+            today = datetime.datetime.strptime(fecha, "%Y-%m-%d").date()
+        except:
+            return "La fecha tiene un formato no valido", 403
+    
+        apiGarmin.setParams(usuario, password)
+        
+        api = apiGarmin.getApi()
+        try:
+            if (api.login() == False):
+                return "Error al loguearse a Garmin", 403
+        except:
+            return "Error inesperado al loguearse a Garmin" + apiGarmin.getParams(), 403
+    
+        yield "Obteniendo contactos\n"
+        
+        try:
+            connections = api.get_connections()
+        except:
+            return "Error al obtener contactos", 403
+    
+        if (not(connections) or not(len(connections)>0) or not('userConnections' in connections) ):
+            return "Error al procesar contactos", 403
+    
+        userConnections = connections['userConnections']
+
+        lastweek = today - datetime.timedelta(days=7)
+        data = {'Usuario':'','Actividades':0, 'Duracion':0}
+    
+        date_list = [today - datetime.timedelta(days=x) for x in range(7)]
+        dates = {'Usuario' : ''}
+        for d in date_list:
+            dates[d] = 0
+    
+        yield "Obteniendo actividades del usuario"
+        
+        try:
+            activities = api.get_activities(1,25)
+        except:
+            return "Error al obtener mis actividades", 403
+    
+        if (not(activities) or not(len(activities)>0)):
+            return "Error al comenzar a procesar actividades", 403
+    
+        try:
+            data['Usuario'] = api.get_full_name()
+            dates['Usuario'] = api.get_full_name()
+        except:
+            return "Error al obtener mi nombre completo", 403
+
+        dur = 0
+        for activitie in activities:
+            datetime_object = datetime.datetime.strptime(activitie['startTimeLocal'], '%Y-%m-%d %H:%M:%S')
+            if (datetime_object.date() in dates):
+                dates[datetime_object.date()] = dates[datetime_object.date()] + 1
+            if (today >= datetime_object.date() > lastweek):
+                data['Actividades'] = data['Actividades'] + 1
+                dur = dur + activitie['duration']
+    
+        data['Duracion'] = datetime.timedelta(seconds=dur)
+    
+        cache['dates'] = []
+        cache['dates'].append(dates)
+    
+        cache['data'] = []
+        cache['data'].append(data)
+
+        for user in userConnections:    
+    
+            data = {'Usuario':'','Actividades':0, 'Duracion':0}
+        
+            date_list = [today - datetime.timedelta(days=x) for x in range(7)]
+            dates = {'Usuario' : ''}
+            for d in date_list:
+                dates[d] = 0
+    
+            try:
+                data['Usuario'] = user['fullName']
+                dates['Usuario'] = user['fullName']
+            except:
+                return "Error al obtener nombre completo del usuario numero " + str(user), 403
+    
+            yield "Obteniendo actividades de " + user['fullName']
+            
+            try:
+                activities = api.get_connection_activities(user['displayName'],1,25)
+            except:
+                return "Error al obtener actividades de " + user['fullName'], 403
+    
+            if (not(activities) or not(len(activities)>0) or not('activityList' in activities) ):
+                return "Error al comenzar a procesar actividades de " + user['fullName'], 403
+    
+            dur = 0    
+            for activitie in activities['activityList']:
+                datetime_object = datetime.datetime.strptime(activitie['startTimeLocal'], '%Y-%m-%d %H:%M:%S')
+                if (datetime_object.date() in dates):
+                    dates[datetime_object.date()] = dates[datetime_object.date()] + 1
+                if (today >= datetime_object.date() > lastweek):
+                    data['Actividades'] = data['Actividades'] + 1
+                    dur = dur + activitie['duration']
+        
+            data['Duracion'] = datetime.timedelta(seconds=dur)
+            
+            cache['dates'].append(dates)
+            cache['data'].append(data)
+
+        api.logout
+    
+        data = cache['data']
+        df = pd.DataFrame(data)
+        df.sort_values(by=['Actividades','Duracion'], inplace=True, ascending=False)
+    
+        result = df.to_html()
+        cache['data'] = []
+
+        yield result   
+
+    return app.response_class(generate(), mimetype="text/plain")
+    
+
 if __name__ =="__main__":
     app.run(debug=True, port=8080)
     
@@ -402,3 +550,5 @@ def test():
 
     result = df.to_html()
     return result
+
+
